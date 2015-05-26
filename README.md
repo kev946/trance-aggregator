d2cea7abc3333dc3b43813dd70386c70b19172e6
Kevin Peters <kevin.peters@tradeforecaster.com>
2015-05-25 22:05:20 -0500

    Added JPlayer, current-track GET route requested periodically from html.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+# Need to get youtube-dl
 sudo curl https://yt-dl.org/downloads/2015.05.15/youtube-dl -o /usr/local/bin/youtube-dl
 sudo chmod 755 /usr/local/bin/youtube-dl
+
+# Need ffmpeg for mp3 conversion
+sudo add-apt-repository ppa:mc3man/trusty-media
+sudo apt-get update
+sudo apt-get install ffmpeg
--- routes/index.js
+++ routes/index.js
@@ -5,42 +5,54 @@ var request = require('request');
 var process = require('child_process');
 
 router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/current-track', function(req, res, next) {
   getSongName(function(err, songName) {
-    if (!err) res.render('index', { title: 'Trance Aggregator', song: songName });
+    if (!err) res.send(songName);
   });
 });
 
 router.post('/', function(req, res) {
-  if (req.body.song) getSongNameYouTubeVideoId(req.body.song, function(err, videoId) {
-    if (!err) downloadConvertVideoIdToMp3(videoId, function(err, result) {
+  if (req.body.song) getYouTubeVideoId(req.body.song, function(err, videoId) {
+    if (!err) downloadYouTubeVideo(videoId, function(err, result) {
       if (!err) res.send(err);
       else res.send(result);
     });
   });
+  res.end();
 });
 
 function getSongName(fn) {
-  var options = {
-    url: 'http://ch01a.320.trance.fm/7.html',
-    headers: {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0'
-    }
-  };
-  request(options, function(err, res, body) {
+// trance.fm
+//  var options = {
+//    url: 'http://ch01a.320.trance.fm/7.html',
+//    headers: {
+//      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0'
+//    }
+//  };
+//  request(options, function(err, res, body) {
+//    if (!err && res.statusCode == 200) {
+//      var csvStreamInfo = body.split('<body>').pop().replace('</body></html>', '');
+//      var delimeter = ',';
+//      var tokens = csvStreamInfo.split(delimeter).slice(6);
+//      var songName = tokens.join(delimeter);
+//      fn(null, songName);
+//    } else {
+//      fn(err);
+//    }
+//  });
+  request('http://api.audioaddict.com/v1/di/track_history', function(err, res, body) {
     if (!err && res.statusCode == 200) {
-      var csvStreamInfo = body.split('<body>').pop().replace('</body></html>', '');
-      var delimeter = ',';
-      var start = 6;
-      var tokens = csvStreamInfo.split(delimeter).slice(start);
-      var songName = tokens.join(delimeter);
-      fn(null, songName);
-    } else {
-      fn(err);
+      var jsonObj = JSON.parse(body);
+      track = jsonObj['1'].track;
+      fn(null, track); 
     }
   });
 }
 
-function getSongNameYouTubeVideoId(songName, fn) {
+function getYouTubeVideoId(songName, fn) {
   var youTube = new YouTube();
   var maxResults = 1;
   youTube.setKey('AIzaSyB1OOSpTREs85WUMvIgJvLTZKye4BVsoFU');
@@ -50,7 +62,8 @@ function getSongNameYouTubeVideoId(songName, fn) {
   });
 }
 
-function downloadConvertVideoIdToMp3(videoId, fn) {
+function downloadYouTubeVideo(videoId, fn) {
+  // download video and convert to mp3
   process.exec('youtube-dl --extract-audio --audio-format=mp3 https://www.youtube.com/watch?v=' + videoId, function callback(error, stdout, stderr) {
     fn(null, stdout);
   });
